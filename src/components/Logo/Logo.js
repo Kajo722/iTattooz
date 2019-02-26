f20c66bc500eba261959d7167e7b19e4533ebfb8
Karol Michalak <karol.michalak93@gmail.com>
2019-02-26 17:41:32 +0100

    header animations implementation

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, {Component} from 'react';
 import styles from './Logo.module.css'
 
-const logo = (props) => {
-    return (
-        <div className={props.logo}>
-            iTatt<span className={styles.span}>o</span>oz
-        </div>
-    )
+const debounce = (func, wait) => {
+    let timeout
+    return (...args) => {
+      clearTimeout(timeout)
+      timeout = setTimeout(() => func.apply(this, args), wait)
+    }
+  }
+class Logo extends Component {
+
+    state = {
+    scrollPositionY: 0,  
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', debounce(this.handleScroll, 16))
+    }
+
+    componentWillUnmount(){
+        window.removeEventListener('scroll', debounce(this.handleScroll, 16))
+    }
+
+    handleScroll = () => {
+        // + is unary operator, same as Number(window.scrollY)
+        const scrollPositionY = +window.scrollY
+        return this.setState({ scrollPositionY })
+      }
+
+    render() {
+        const isScrolling = !!this.state.scrollPositionY
+        return (
+            <div className={isScrolling ? styles.logoSmall : styles.logo}>
+                iTatt<span className={styles.span}>o</span>oz
+            </div>
+        )
+    }
 }
 
-export default logo;
+export default Logo;
